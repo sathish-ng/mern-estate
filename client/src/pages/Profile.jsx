6c249de0a8d11567d8862c6beddd3b594b46c174
sathish-ng <kumarsathish27208@gmail.com>
2025-01-02 12:56:34 +0530

    Create update user api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,9 @@
 import { useSelector } from "react-redux"
+import { useRef } from "react"
+
 
 export default function Profile() {
+  const fileRef =useRef(null)
 
   const {currentUser} =useSelector((state) => state.user)
 
@@ -9,7 +12,9 @@ export default function Profile() {
       <h1 className='text-3xl font-semibold text-center
       my-7'> Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt='profile' 
+        <input type="file" ref={fileRef} hidden 
+        accept="image/*"/>
+        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar} alt='profile' 
         className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"/>
         <input type="text" placeholder="username"
         id='username' className="border p-3 rounded-lg"/>

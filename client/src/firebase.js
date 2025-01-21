1afadfb845428d499b4d988dc0b60d745e860323
sathish-ng <kumarsathish27208@gmail.com>
2025-01-21 12:18:00 +0530

    Complete Upload listing images Functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,23 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIRREBASE_API_KEY,
+//   authDomain: "mern-estate-f947a.firebaseapp.com",
+//   projectId: "mern-estate-f947a",
+//   storageBucket: "mern-estate-f947a.firebasestorage.app",
+//   messagingSenderId: "162105252921",
+//   appId: "1:162105252921:web:cadb87de0eaddf5fe2c1e8"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,11 +26,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIRREBASE_API_KEY,
-  authDomain: "mern-estate-f947a.firebaseapp.com",
-  projectId: "mern-estate-f947a",
-  storageBucket: "mern-estate-f947a.firebasestorage.app",
-  messagingSenderId: "162105252921",
-  appId: "1:162105252921:web:cadb87de0eaddf5fe2c1e8"
+  authDomain: "mern-estate1-b8226.firebaseapp.com",
+  projectId: "mern-estate1-b8226",
+  storageBucket: "mern-estate1-b8226.firebasestorage.app",
+  messagingSenderId: "301833027725",
+  appId: "1:301833027725:web:8610a1ce13abeeff3ae2c4"
 };
 
 // Initialize Firebase
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,28 @@
 import { useSelector } from "react-redux"
-import { useRef, useState } from "react"
+import { useRef, useState ,useEffect} from "react"
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart} from '../redux/user/userSlice'
 import { useDispatch } from "react-redux"
 import {Link} from 'react-router-dom'
+import { app } from "../firebase";
 
 
 export default function Profile() {
   const fileRef =useRef(null)
 
   const {currentUser, loading , error} =useSelector((state) => state.user)
+  const [file, setFile] =useState(undefined)
+  console.log(file);
+
+  const [filePerc, setFilePerc] =useState(0);
+  const [fileUploadError, setFileUploadError] =useState(false);
+
   const [formData,setFormData] =useState({})
+  
+  // console.log(formData)
+  // console.log(filePerc)
+  // console.log(fileUploadError)
+
   const [updateSuccess, setUpdateSuccess] =useState(false);
   const dispatch = useDispatch();
 
@@ -79,6 +92,39 @@ export default function Profile() {
           }
       }
 
+      useEffect(()=>{
+        if(file){
+          handleFileUpload(file);
+        }
+      },[file]);
+
+      const handleFileUpload =(file) =>{
+        const storage =getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef =ref(storage,fileName)
+        const uploadTask =uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+          (snapshot)=>{
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) *100;
+            setFilePerc(Math.round(progress))
+          },
+        (erroe)=>{
+          setFileUploadError(true);
+        },
+        ()=>{
+          getDownloadURL(uploadTask.snapshot.ref).then
+          ((downloadURL) => setFormData({...formData, avatar: downloadURL})
+        );
+          }
+        )
+        }
+
+        //firebase to storage
+        // allow read;
+        // allow write: if
+        // request.resource.size < 2 * 1024 *1024 &&
+        // request.resource.contentType.matches('image/.*')
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -86,10 +132,24 @@ export default function Profile() {
       my-7'> Profile</h1>
       
       <form  onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="file" ref={fileRef} hidden 
+        <input  onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden 
         accept="image/*"/>
+        
         <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt='profile' 
         className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"/>
+     
+      <p className="text-sm self-center">
+        {fileUploadError ? (
+          <span className="text-red-700">Error image Upload(Image must be less than 2 mb)</span>
+        ) : filePerc > 0 && filePerc < 100 ? (
+          <span className="text-slate-700"> {`Uploading ${filePerc}%`} </span>
+        ) : filePerc === 100 ? (
+          <span className="text-green-700">Image Successfully Uploaded!</span>
+        ) : (
+          ""
+        )}
+      </p>
+
         
         <input type="text" 
           placeholder="username"

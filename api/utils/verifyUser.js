6d8c9d074ca17fc80b24f2dcb280f3938a71569a
sathish-ng <kumarsathish27208@gmail.com>
2025-01-23 13:34:42 +0530

    Complete create listing page functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,16 +1,15 @@
-import jwt from 'jsonwebtoken'
-import {errorHandler} from './error.js'
+import jwt from 'jsonwebtoken';
+import { errorHandler } from './error.js';
 
-export const verifyToken = (req,res,next)=>{
-    const token =req.cookies.access_token;
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
 
-    if(!token) return next(errorHandler(401, 'Unauthorized'))
+  if (!token) return next(errorHandler(401, 'Unauthorized'));
 
-    jwt.verify(token, process.env.JWT_SECRET, (err ,user)=>{
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, 'Token is not Vaild'));
 
-        if(err) return next(errorHandler(403, "token is not valid"));
-
-        req.user = user;
-        next();
-    });
-}
+    req.user = user;
+    next();
+  });
+};

081ed09b7ec5fce7a1eae1c4f16d95e8994c75f3
sathish-ng <kumarsathish27208@gmail.com>
2025-01-24 12:53:16 +0530

    Complete Show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -24,6 +24,10 @@ export default function Profile() {
   // console.log(fileUploadError)
 
   const [updateSuccess, setUpdateSuccess] =useState(false);
+
+  const [showListingsError, setShowListingsError] =useState(false)
+  const [userListings, setUserListings] = useState([])
+
   const dispatch = useDispatch();
 
 
@@ -126,6 +130,25 @@ export default function Profile() {
         // request.resource.size < 2 * 1024 *1024 &&
         // request.resource.contentType.matches('image/.*')
 
+        const handleShowListings =async () =>{
+          try {
+
+            setShowListingsError(false)
+            const res = await fetch(`/api/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (data.success === false)
+            {
+              setShowListingsError(true);
+              return;
+            }
+
+            setUserListings(data);
+
+          } catch (error) {
+            setShowListingsError(true);
+          }
+        }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center
@@ -188,6 +211,52 @@ export default function Profile() {
 
       <p className="text-red-700 mt-5">{error ? error :""}</p>
       <p className="text-green-700 mt-5">{updateSuccess ? 'User is Updated Successfully' : ''}</p>
-    </div>
-  )
+      
+      <button className="text-green-700 w-full" onClick={handleShowListings}> 
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? 'Error in showing listings' : ''}
+      </p>
+
+        {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+        <h1 className='text-center mt-7 text-2xl font-semibold'>
+          Your Listings
+        </h1>
+        {userListings.map((listing) => (
+          <div
+            key={listing._id}
+            className='border rounded-lg p-3 flex justify-between items-center gap-4'
+          >
+            <Link to={`/listing/${listing._id}`}>
+              <img
+                src={listing.imageUrls[0]}
+                alt='listing cover'
+                className='h-16 w-16 object-contain'
+              />
+            </Link>
+            <Link
+              className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+              to={`/listing/${listing._id}`}
+            >
+              <p>{listing.name}</p>
+            </Link>
+
+            <div className='flex flex-col item-center'>
+              <button
+                className='text-red-700 uppercase'
+              >
+                Delete
+              </button>
+              <Link>
+                <button className='text-green-700 uppercase'>Edit</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    )}
+  </div>
+);
 }
